73414d0367b7e453221e532e7e5cf932dfa74a61
minkyungCHO <mk981015@naver.com>
2025-01-03 19:56:00 +0900

    feat: w02-13 if-else if-else문

---
+++ src/week02/w14.java
@@ -0,0 +1,16 @@
+package week02;
+
+public class w14 {
+    public static void main(String[] args) {
+        // 조건문 with else if
+        int number = 2;
+
+        if (number == 1) {
+            System.out.println("number 값은 1 입니다."); // 미출력
+        } else if (number == 2) {
+            System.out.println("number 값은 2 입니다."); // number 값은 2 입니다. 출력
+        } else {
+            System.out.println("number 값은 모르는 값입니다."); // 미출력
+        }
+    }
+}
