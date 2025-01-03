cf6eb8d80423328bb74ed3bb7f380e855f997d44
minkyungCHO <mk981015@naver.com>
2025-01-03 16:56:46 +0900

    feat: w02-7 형변환 연산

--- src/week02/w07.java
+++ src/week02/w07.java
@@ -1,8 +1,10 @@
 package week02;
 
 public class w07 {
-    // 형변환 연산자
-    int intNumber = 93 + (int) 98.8; // 93 + 98
+    public static void main(String[] args) {
+        // 형변환 연산자
+        int intNumber = 93 + (int) 98.8; // 93 + 98
 
-    double doubleNumber = (double) 93 + 98.8; // 93.0 + 98.8
+        double doubleNumber = (double) 93 + 98.8; // 93.0 + 98.8
+    }
 }
