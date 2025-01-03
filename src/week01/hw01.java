a4bd9b068fa5d4168eb2e77fb5cc55fc1d582b22
minkyungCHO <mk981015@naver.com>
2025-01-03 15:44:50 +0900

    chore: 폴더 정리

--- src/week01/hw01.java
+++ src/week01/hw01.java
@@ -1,8 +1,8 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+package week01;//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 import java.util.Scanner;
 
-public class Main {
+public class hw01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
---
+++ src/week02/w01.java
@@ -0,0 +1,9 @@
+package week02;
+
+
+public class w01 {
+
+    public static void main(String[] args) {
+
+    }
+}
