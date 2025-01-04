d6e90d17cd50eb4fd8423d53bd782206beb2a42b
minkyungCHO <mk981015@naver.com>
2025-01-04 18:04:51 +0900

    feat: w02-31 배열 순회

---
+++ src/week02/w31.java
@@ -0,0 +1,18 @@
+package week02;
+
+public class w31 {
+    public static void main(String[] args) {
+        //길이가 3인 정수배열 선언
+        int[] intArray = new int[3];
+
+        // 배열이 초기화된 값을 출력해본다.
+        for(int i=0; i<intArray.length; i++) { // .length 는 배열의 길이
+            System.out.println(intArray[i]);
+        }
+
+        // 출력
+        // 0
+        // 0
+        // 0
+    }
+}
