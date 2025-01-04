721253b1d352f364c02951479d4a6cd49a08557e
minkyungCHO <mk981015@naver.com>
2025-01-04 16:10:15 +0900

    feat: w02-23 break 명령

---
+++ src/week02/w23.java
@@ -0,0 +1,19 @@
+package week02;
+
+public class w23 {
+    public static void main(String[] args) {
+        // break 명령
+
+        int number = 0;
+        while(number < 3) {
+            number++;
+            if (number == 2) {
+                break;  // 2일때 반복 중단
+            }
+            System.out.println(number + "출력");
+        }
+
+        // 출력
+        // 1출력
+    }
+}
