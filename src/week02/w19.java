d62625d4c9d6ccf80b3b956ed5987bbd5407e1e8
minkyungCHO <mk981015@naver.com>
2025-01-04 15:51:59 +0900

    feat: w02-19 for 문

---
+++ src/week02/w19.java
@@ -0,0 +1,17 @@
+package week02;
+
+public class w19 {
+    public static void main(String[] args) {
+        // for 문
+
+        for(int i=0; i < 4; i++) { // 변수 i 값은 0 ~ 3 까지 반복
+            System.out.println(i + "번째 출력"); // i 변수와 문자열 합치기
+        }
+
+        // 출력
+        // 0번째 출력
+        // 1번째 출력
+        // 2번째 출력
+        // 3번째 출력
+    }
+}
