e1cf4bd321ab7306654a91af600cfc0bb47b3400
minkyungCHO <mk981015@naver.com>
2025-01-04 15:28:08 +0900

    feat: w02-13 if-else문

---
+++ src/week02/w13.java
@@ -0,0 +1,14 @@
+package week02;
+
+public class w13 {
+    public static void main(String[] args) {
+        // 조건문 with else
+        boolean flag = false;
+
+        if (flag) {
+            System.out.println("flag 값은 true 입니다."); // 미출력
+        } else {
+            System.out.println("flag 값은 false 입니다."); // flag 값은 false 입니다. 출력
+        }
+    }
+}
