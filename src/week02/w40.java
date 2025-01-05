2112c80f94c4db9e035a2338b7d5499986245c7c
minkyungCHO <mk981015@naver.com>
2025-01-05 22:09:35 +0900

    feat: w02-40 최대값 구하기

---
+++ src/week02/w40.java
@@ -0,0 +1,22 @@
+package week02;
+
+public class w40 {
+    public static void main(String[] args) {
+        // 최대값 구하기
+
+        int[] arr = { 3, 2, 1, 5, 1 };
+
+        // 최대값 초기값 세팅
+        int max = arr[0];
+
+        // 최대값 구하기
+        for (int num : arr) {
+            if (num > max) { // 반복문 돌면서 나(max)보다 값이 작으면 저장
+                max = num;
+            }
+        }
+
+        // 최대값 5 출력
+        System.out.println(max);
+    }
+}
