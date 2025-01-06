8ee93795872704b2b03ca3c52a21a0057c9e19e0
minkyungCHO <mk981015@naver.com>
2025-01-06 14:58:14 +0900

    feat: w03 package, import

--- src/week03/packageExample/main/Main.java
+++ src/week03/packageExample/main/Main.java
@@ -1,8 +1,10 @@
 package week03.packageExample.main;
 
+import week03.packageExample.pk1.Car;
+
 public class Main {
     public static void main(String[] args) {
-        week03.packageExample.pk1.Car car = new week03.packageExample.pk1.Car();
+        Car car = new Car();
         car.horn(); // pk1 빵빵
 
         week03.packageExample.pk2.Car car2 = new week03.packageExample.pk2.Car();
